eba5c748d63b1d235d16b5dc5d396524a6c3b781
hemamane <hemamane5376@gmail.com>
2022-06-30 17:45:05 +0530

    pattern assignment

--- hema.js
+++ hema.js
@@ -1,31 +1,31 @@
 
-// let num = 5;
+let num = 5;
 
-// for(let i = 1; i<=5; i++){
-//   let stars = ``
-//   for(let j = 1; j<=5; j++){
-//   if(i==j||(i+j==6)){
-//      stars += "   *    "
-//   }
-//   else{
-//     stars += "       "
-//   }
+for(let i = 1; i<=5; i++){
+  let stars = ``
+  for(let j = 1; j<=5; j++){
+  if(i==j||(i+j==6)){
+     stars += "   *    "
+  }
+  else{
+    stars += "       "
+  }
   
   
-//   }console.log(stars)
-// }
+  }console.log(stars)
+}
 
 
-// let num = 5;
-// for(let i = 1; i<=5; i++){
-//   stars = ``
-//   for(let j = 1; j<=5; j++){
-//     if(i==(j+2)||j==5){
-//        stars += "      *      "
-//     }else stars +="   "
+let num = 5;
+for(let i = 1; i<=5; i++){
+  stars = ``
+  for(let j = 1; j<=5; j++){
+    if(i==(j+2)||j==5){
+       stars += "      *      "
+    }else stars +="   "
    
-//   }console.log(stars)
-// }
+  }console.log(stars)
+}
 
 
 
